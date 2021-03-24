8ef9174a5a425275b7c3e1d263e0e22bf90beead
Cameron Brown <hello@cameronbrown.co.uk>
2021-03-24 13:59:52 +0000

    add board sizes from adaconfig ini

--- config.cc
+++ config.cc
@@ -1,4 +1,7 @@
 #include "config.h"
+#include <fstream>
+#include <regex>
+#include <string>
 
 namespace google {
 
@@ -6,6 +9,28 @@ GameModeConfig LoadConfigFromFile() {
   GameModeConfig _config;
   _config.boardWidth = 10;
   _config.boardHeight = 10;
+
+  std::string buffer;
+  std::ifstream configFile("adaship_config.ini");
+  while (getline (configFile, buffer)) {
+      if (buffer.rfind("Board: ", 0) == 0) {
+      // Adapted from: https://stackoverflow.com/questions/33520934/regex-with-only-numbers-in-a-string-c
+      std::regex rx(R"((?:^|\s)([+-]?[[:digit:]]+(?:\.[[:digit:]]+)?)(?=$|\s))");
+      std::smatch m;
+      std::string str = buffer;
+
+      int i = 0;
+      while (regex_search(str, m, rx)) {
+          if (i == 0) _config.boardWidth = std::atoi(std::string(m[1]).c_str());
+          if (i == 1) _config.boardHeight = std::atoi(std::string(m[1]).c_str());
+          str = m.suffix().str();
+          i++;
+      }
+    }
+  }
+
+  configFile.close();
+
   _config.ships.push_back(ShipDefinition{
     .length = 5,
     .name = "Carrier",
@@ -29,4 +54,4 @@ GameModeConfig LoadConfigFromFile() {
   return _config;
 }
 
-}
+} // namespace google
